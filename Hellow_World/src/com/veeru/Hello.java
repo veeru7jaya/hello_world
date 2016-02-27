ea9eabb79dea2a09b37de691666f03ea35857c2b
veeru <veeru7jaya@gmail.com>
2016-02-27 14:27:39 +0530

    next commmitt

--- Hellow_World/src/com/veeru/Hello.java
+++ Hellow_World/src/com/veeru/Hello.java
@@ -2,7 +2,7 @@ package com.veeru;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello.....!!!");
+		System.out.println("Hello : Jayaveera .....!!!");
 	}
 
 }
